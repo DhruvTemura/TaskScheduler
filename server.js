e15d07bd369608666da5fd21d377e560c4f38e49
DhruvTemura <dhruvtemura@gmail.com>
2025-10-17 18:57:39 +0530

    basic setup - server.js

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 //this file handles all our HTTP requests (main server file)
 
+const express = require('express')
+const taskManager = require('./taskManager')
+
+const app = express()
+const PORT = process.env || 3000
+
